c5c5c1f9aa2f8f2bc5df1b25ea7c5e115c66ef15
byilagan <ilaganbgm@gmail.com>
2019-03-19 20:28:40 -0700

    Added brand home link

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'; 
 import brand from '../images/logo.png';
+import {Link} from 'react-router-dom'
 
 class SearchBar extends React.Component {
     render () {
         return (
             <div id="searchBar">
-                <img src={brand}/> 
+                <Link to="/"><img src={brand}/></Link>
                 <input placeholder="Search"/>
             </div>
         )
